f5a39966e9780e7d3cfa23aa556b1af92b11f050
Mario Burgos <mario.burpi@gmail.com>
2023-02-01 21:51:33 +0100

    database schema

---
+++ src/test/resources/sql-database/airline.sql
@@ -0,0 +1,26 @@
+DROP SCHEMA IF EXISTS lab_three_o_four;
+CREATE SCHEMA lab_three_o_four;
+USE lab_three_o_four;
+--
+
+CREATE TABLE aircraft (
+    model VARCHAR(255) NOT NULL UNIQUE,
+    total_seats INT,
+    PRIMARY KEY (model)
+);
+
+CREATE TABLE customer (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    status VARCHAR(255),
+    PRIMARY KEY (id),
+);
+
+CREATE TABLE flight (
+    number VARCHAR(255) NOT NULL,
+    mileage INT NOT NULL,
+    aircraft_model VARCHAR(255) NOT NULL,
+    PRIMARY KEY (flight_number),
+    FOREIGN KEY (aircraft_model)
+        REFERENCES aircraft (model)
+);
